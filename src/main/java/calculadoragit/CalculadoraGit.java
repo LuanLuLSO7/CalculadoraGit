6e9056f0932a6c30be5b0b4cfe1143671ef0823d
luanl <luanl@LuanLuL-COMPUTER>
2021-11-09 22:17:28 -0300

    cdg Classcalculador OK

--- src/main/java/calculadoragit/CalculadoraGit.java
+++ src/main/java/calculadoragit/CalculadoraGit.java
@@ -5,17 +5,55 @@
  */
 package calculadoragit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author luanl
  */
 public class CalculadoraGit {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    static int x, y;
+    static int soma;
+    static int subtracao;
+    static int multiplicacao;
+    static float divisao;
+            
+    public static void soma(){
+        soma=x+y;
+        System.out.println("Soma= "+ soma + "\n");
+    }
+    
+    public static void subtracao(){
+        subtracao=x-y;
+        System.out.println("Subtração= "+ subtracao  + "\n");
+    }
+    
+    public static void multiplicacao(){
+        multiplicacao=x*y;
+        System.out.println("Multiplicação= "+ multiplicacao + "\n");
+    }
+    
+    public static void divisao(){
+        divisao=(float)x/y;
+        System.out.println("Divisão= "+ divisao + "\n");
+    }
+   
+    public static void main(String[] args) {      
+       
+        Scanner scn= new Scanner(System.in);
+        
+        System.out.println("Digite o valor de x: ");
+        x=scn.nextInt();
+        System.out.println("Digite o valor de y: ");
+        y=scn.nextInt();
+        
+        System.out.println("");
+        
+        soma();
+        subtracao();
+        multiplicacao();
+        divisao();
     }
     
 }
