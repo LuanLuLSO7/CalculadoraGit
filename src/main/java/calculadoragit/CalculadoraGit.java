6d42c0b74cb0e012e4d6ee4c6609f0948d19d55b
luanl <luanl@LuanLuL-COMPUTER>
2021-11-10 19:11:01 -0300

    cdg Instaciação de Objetos

--- src/main/java/calculadoragit/CalculadoraGit.java
+++ src/main/java/calculadoragit/CalculadoraGit.java
@@ -1,50 +1,67 @@
 package calculadoragit;
 
-import java.util.Scanner;
-
 public class CalculadoraGit {
 
-    static int x, y;
-    static int soma;
-    static int subtracao;
-    static int multiplicacao;
-    static float divisao;
+    private int x, y;
+    private int soma;
+    private int subtracao;
+    private int multiplicacao;
+    private float divisao; 
+    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
             
-    public static void soma(){
+    public void soma(){
         soma=x+y;
         System.out.println("Soma= "+ soma + "\n");
     }
     
-    public static void subtracao(){
-        subtracao=x-y;
-        System.out.println("Subtração= "+ subtracao  + "\n");
+    public int soma(int x, int y){
+        int soma=x+y;
+        return soma;
     }
     
-    public static void multiplicacao(){
+    public void subtracao(){
+        subtracao=x-y;
+        System.out.println("Subtração= "+ subtracao + "\n");
+    }
+    
+    public int subtracao(int x, int y){
+        int subtracao=x-y;
+        return subtracao;
+    }
+     
+    public void multiplicacao(){
         multiplicacao=x*y;
         System.out.println("Multiplicação= "+ multiplicacao + "\n");
     }
     
-    public static void divisao(){
+    public int multiplicacao(int x, int y){
+        int multiplicacao=x*y;
+       return multiplicacao;
+    }
+    
+    public void divisao(){
         divisao=(float)x/y;
         System.out.println("Divisão= "+ divisao + "\n");
     }
-   
-    public static void main(String[] args) {      
-       
-        Scanner scn= new Scanner(System.in);
-        
-        System.out.println("Digite o valor de x: ");
-        x=scn.nextInt();
-        System.out.println("Digite o valor de y: ");
-        y=scn.nextInt();
-        
-        System.out.println("");
-        
-        soma();
-        subtracao();
-        multiplicacao();
-        divisao();
+    
+    public float divisao(int x, int y){
+        float divisao=(float)x/y;
+        return divisao;
     }
     
 }
